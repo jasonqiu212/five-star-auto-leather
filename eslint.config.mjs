9c049c1f3eb18f4e63d858f01ff125c7014920a8
jason.qiujs <jason.qiujs@shopee.com>
2025-10-25 18:45:05 +0800

    feat: add more prettier and eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,18 @@
-import eslintPluginAstro from "eslint-plugin-astro";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
 
 export default [
   ...eslintPluginAstro.configs.recommended,
   {
     plugins: {
+      '@typescript-eslint': typescriptEslintPlugin,
       prettier: eslintPluginPrettier,
     },
     rules: {
-      "prettier/prettier": "error",
+      'prettier/prettier': 'error',
+
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
   },
 ];
